a4fec14c931a2176dff55df746e12a346a62ac46
Kaique da Silva <kaique.developer@gmail.com>
2015-04-12 17:03:58 -0300

    Added filter to total spendings Added button to see all list Do not show empty fields

--- expenses.js
+++ expenses.js
@@ -1,4 +1,5 @@
 var Spendings = new Mongo.Collection('spendings');
+var SpendingsArchive = new Mongo.Collection('spendingsArchive');
 
 if (Meteor.isClient) {
   angular
@@ -20,7 +21,13 @@ if (Meteor.isClient) {
 
     .controller('ExpensesCtrl', ['$scope', '$meteor', '$log', function ($scope, $meteor, $log) {
       $scope.expenses = {};
-      $scope.spendings = $meteor.collection(Spendings).subscribe('spendings');
+      $scope.spendings = $meteor.collection(function () { 
+        return Spendings.find({}, { sort: { createdAt: -1 } }); 
+      }).subscribe('spendings');
+      $scope.spendingsArchive = $meteor.collection(SpendingsArchive);
+      $scope.currentMerchant = "";
+      $scope.currentCategory = "";
+      $scope.currentLocation = "";
 
       $scope.addExpense = function (expenses) {
         $log.debug('add expense');
@@ -39,15 +46,51 @@ if (Meteor.isClient) {
         return $scope.spendings;
       };
 
-      $scope.removeSpending = function (spending) {
-        $log.debug('remove spending');
+      $scope.archiveSpending = function (spending) {
+        $log.debug('archive spending');
+        $scope.spendingsArchive.save({
+          value: spending.value,
+          merchant: spending.merchant,
+          location: spending.location,
+          category: spending.category,
+          note: spending.note,
+          createdAt: spending.createdAt,
+          updatedAt: Date.now()
+        });
         $scope.spendings.remove(spending);
       };
+
+      $scope.setCurrentMerchant = function (merchant) {
+        $log.debug('set current merchant');
+        $scope.currentMerchant = merchant;
+      };
+
+      $scope.setCurrentCategory = function (category) {
+        $log.debug('set current category');
+        $scope.currentCategory = category;
+      };
+
+      $scope.setCurrentLocation = function (location) {
+        $log.debug('set current location');
+        $scope.currentLocation = location;
+      };
     }])
+
+    .directive('listControls', function () {
+      return {
+        templateUrl: 'list-controls.ng.html'
+      };
+    })
 
     .directive('spendings', function () {
       return {
         templateUrl: 'spendings.ng.html'
+      };
+    })
+
+    .directive('totalSpendings', function () {
+      return {
+        templateUrl: 'total-spendings.ng.html'
       };
     })
 
@@ -60,6 +103,29 @@ if (Meteor.isClient) {
     .directive('addSpending', function () {
       return {
         templateUrl: 'add-spending.ng.html'
+      };
+    })
+
+    .filter('totalSpendings', function () {
+      return function (input) {
+        var out = 0;
+        var values = input.map(function (spending) {
+          return spending.value;
+        });
+
+        function sum(input) {
+          var total = 0;
+
+          input.map(function (value) {
+            total += value;
+          });
+
+          return total;
+        }
+
+        out = sum(values);
+
+        return out;
       };
     })
     
