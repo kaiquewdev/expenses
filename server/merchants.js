3bec393af10a74c2ec932cab039e317447a3a8fb
Kaique da Silva <kaique.developer@gmail.com>
2015-04-14 01:55:10 -0300

    fix merchants

--- server/merchants.js
+++ server/merchants.js
@@ -1,3 +1,3 @@
 Meteor.publish('merchants', function () {
-  return Merchants.find({ $and: [{ createdBy: this.userId }, { createdBy: { $exists: true } }] }, { sort: { rank: -1 } });
+  return Merchants.find({}, { sort: { rank: -1 } });
 });
